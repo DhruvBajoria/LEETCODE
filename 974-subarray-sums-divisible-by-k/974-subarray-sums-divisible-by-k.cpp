216339c7ff769e037e583a32bfaa7e2ccf6982bd
Dhruv <71303408+DhruvBajoria@users.noreply.github.com>
2022-05-18 15:35:39 +0530

    Time: 92 ms (32.70%), Space: 32 MB (9.45%) - LeetHub

--- 974-subarray-sums-divisible-by-k/974-subarray-sums-divisible-by-k.cpp
+++ 974-subarray-sums-divisible-by-k/974-subarray-sums-divisible-by-k.cpp
@@ -1,25 +1,46 @@
 class Solution {
 public:
     int subarraysDivByK(vector<int>& nums, int k) {
-        int n=nums.size();
+       int n=nums.size();
         if(n==0)
             return 0;
-       
+        int ans=0;
         map<int,int>m;
+        int currsum=0;
         m[0]=1;
-        int i=0,count=0,currsum=0;
-        while(i<n)
+        for(int i=0;i<n;i++)
         {
             currsum+=nums[i];
             int rem=currsum%k;
             if(rem<0)
                 rem+=k;
             if(m.find(rem)!=m.end())
-                count+=m[rem];
+            {
+                ans+=m[rem];
+            }
             m[rem]++;
-            i++;
-        
         }
-        return count;
+        return ans;
     }
 };
+
+// int n=nums.size();
+//         if(n==0)
+//             return 0;
+       
+//         map<int,int>m;
+//         m[0]=1;
+//         int i=0,count=0,currsum=0;
+//         while(i<n)
+//         {
+//             currsum+=nums[i];
+//             int rem=currsum%k;
+//             if(rem<0)
+//                 rem+=k;
+//             if(m.find(rem)!=m.end())
+//                 count+=m[rem];
+//             m[rem]++;
+//             i++;
+        
+//         }
+//         return count;
