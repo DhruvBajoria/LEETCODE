da56bf448285b74f755a5fc45e489c7cc37b6646
Dhruv <71303408+DhruvBajoria@users.noreply.github.com>
2022-05-23 11:18:08 +0530

    Added solution - LeetHub

--- Isomorphic Strings - GFG/isomorphic-strings.cpp
+++ Isomorphic Strings - GFG/isomorphic-strings.cpp
@@ -13,23 +13,28 @@ class Solution
     //Function to check if two strings are isomorphic.
     bool areIsomorphic(string s1, string s2)
     {
-     int l1=s1.length();
+        int l1=s1.length();
         int l2=s2.length();
+        if(l1!=l2)
+        return false;
         int m1[256]={0};
         int m2[256]={0};
         if(l1!=l2)
         return false;
         for(int i=0;i<l1;i++)
         {
-            if(!m1[s1[i]] and !m2[s2[i]])
+            if(!m1[s1[i]]&&!m2[s2[i]])
             {
                 m1[s1[i]]=s2[i];
                 m2[s2[i]]=s1[i];
             }
-            else if(m1[s1[i]]!=s2[i])
-            return false;
+            else
+            {
+                if(m1[s1[i]]!=s2[i])
+                return false;
+            }
         }
-        return true; // Your code here
+        return true;
         
     }
 };
