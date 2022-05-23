c9d06216bba57f11d7966669dbf14aa62d22cc13
Dhruv <71303408+DhruvBajoria@users.noreply.github.com>
2022-05-23 16:07:30 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (82.63%) - LeetHub

---
+++ 647-palindromic-substrings/647-palindromic-substrings.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int countSubstrings(string s) {
+        int n=s.length();
+        int ans=0;
+        for(int i=0;i<s.length();i++)
+        {
+            int l=i,r=i;
+            while(l>=0&&r<n&&s[l]==s[r])
+            {
+                ans++;
+                l--;
+                r++;
+            }
+            l=i,r=i+1;
+            while(l>=0&&r<n&&s[l]==s[r])
+            {
+                ans++;
+                l--;
+                r++;
+            }
+            
+        }
+        return ans;
+    }
+};
