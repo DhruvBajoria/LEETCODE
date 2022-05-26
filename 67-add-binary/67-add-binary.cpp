be5aa02781e9111c50aa1db5902090a7d3fb2fd6
Dhruv <71303408+DhruvBajoria@users.noreply.github.com>
2022-05-26 12:31:58 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (55.26%) - LeetHub

--- 67-add-binary/67-add-binary.cpp
+++ 67-add-binary/67-add-binary.cpp
@@ -1,40 +1,42 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
-            int la = a.size();
-        int lb = b.size();
-        
-        int l = abs(la-lb);
-        string temp = "";
-        for(int i =0; i < l; i++)
-            temp +='0';
-        
-        string ans = "";
-        if(la<lb)
-            a = temp + a;
-        else b = temp +b;
-        
-        reverse(a.begin(), a.end());
-        reverse(b.begin(), b.end());
-        
-        int carry =0;
-        for(int i = 0; i <max(la, lb); i++)
+   int l1=a.length();
+        int l2=b.length();
+        int l=abs(l1-l2);
+        string temp;
+        for(int i=0;i<l;i++)
         {
-            int x = a[i] -'0', y = b[i] -'0';
-            int r = (x^y)^carry;
-            
-            if(x+y+carry > 1)
-                carry = 1;
-            else carry = 0;
-            
-            ans += r+'0';
+            temp+='0';
         }
-        
+        if(l1>l2)
+        {
+            b=temp+b;
+        }
+        else
+        {
+            a=temp+a;
+        }
+        reverse(a.begin(),a.end());
+        reverse(b.begin(),b.end());
+        int carry=0,r=0;
+        string ans;
+        for(int i=0;i<max(l1,l2);i++)
+        {
+            int x=a[i]-'0';
+            int y=b[i]-'0';
+            r=(x^y)^carry;
+            if(x+y+carry>1)
+                carry=1;
+            else
+                carry=0;
+            ans+=r+'0';
+        }
         if(carry)
-            ans += 1+'0';
-        
-        reverse(ans.begin(), ans.end());
-        
+        {
+            ans+=1+'0';
+        }
+        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
