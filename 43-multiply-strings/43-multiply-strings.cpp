dad9d24f57fcf266fccd84a81a07dd8862c6b1f3
Dhruv <71303408+DhruvBajoria@users.noreply.github.com>
2022-05-26 15:15:02 +0530

    Time: 9 ms (60.69%), Space: 6.7 MB (72.29%) - LeetHub

---
+++ 43-multiply-strings/43-multiply-strings.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    string multiply(string num1, string num2) {
+        int n=num1.size();
+        int m=num2.size();
+        vector<int>ans(n+m,0);
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int j=m-1;j>=0;j--)
+            {
+                int num=(num1[i]-'0')*(num2[j]-'0');
+                int sum=num+ans[i+j+1];
+                ans[i+j]+=sum/10;
+                ans[i+j+1]=sum%10;
+                
+            }
+        }
+        string res="";
+        for(int i=0;i<n+m;i++)
+        {
+            if(res.length()!=0||ans[i]!=0)
+            {
+                res+=ans[i]+'0';
+            }
+        }
+        if(res.length()==0)
+            return "0";
+        return res;
+
+    }
+};
+
+
+
+
+
+//          int m = num1.size();
+//         int n = num2.size();
+        
+//         vector<int> ans((m+n),0);
+        
+//         for(int i = m-1; i>=0; i--){
+//             for(int j = n-1; j>=0; j--){
+//                 int num = (num1[i] - '0') * (num2[j] - '0');
+//                 int sum = ans[i + j + 1] + num;
+//                 ans[i + j] += sum/10;
+//                 ans[i + j + 1] = sum%10;
+//             }
+//         }
+        
+//         string s = "";
+//         for(int i=0; i<(m+n); i++){
+//             if(s.length() != 0 || ans[i] != 0){
+//                 s += (ans[i] + '0');
+//             }
+//         }
+        
+//         if(s.length() == 0){
+//             return "0";
+//         }
+//         return s;
