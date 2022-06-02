a58c2cec5485f365933b5e011cd73d4dbc602018
Dhruv <71303408+DhruvBajoria@users.noreply.github.com>
2022-06-02 13:00:18 +0530

    Time: 123 ms (14.19%), Space: 21 MB (5.19%) - LeetHub

--- 85-maximal-rectangle/85-maximal-rectangle.cpp
+++ 85-maximal-rectangle/85-maximal-rectangle.cpp
@@ -1,46 +1,103 @@
 class Solution {
 public:
-    int maximalrect(vector<int>&dp)
+     vector<int>NSL(vector<int>a,int n)
     {
-        stack<int>st;
-        st.push(-1);
-        int maxi=0;
-        for(int i=0;i<=dp.size();i++)
-        {
-            int val=(i==dp.size())?-1:dp[i];
-            while(st.top()!=-1&&dp[st.top()]>val)
-            {
-                int height=dp[st.top()];
-                st.pop();
-                int width=i-st.top()-1;
-                maxi=max(maxi,width*height);
-            }
-            st.push(i);
-        }
-        return maxi;
-        
-    }
-    int maximalRectangle(vector<vector<char>>& g) {
-        int n=g.size();
-        if(n==0)
-            return 0;
-        int m=g[0].size();
-        if(n+m==2)
-            return g[0][0]=='1';
-        vector<int>dp(m,0);
-        int res=0;
+        vector<int>v;
+        stack<pair<int,int>>st;
         for(int i=0;i<n;i++)
         {
-            for(int j=0;j<m;j++)
+            if(st.size()==0)
+            v.push_back(-1);
+            else if(st.size()>0&&st.top().first<a[i])
             {
-                if(g[i][j]=='0')
-                    dp[j]=0;
-                else
-                     dp[j]++;
+                v.push_back(st.top().second);
             }
-            res=max(res,maximalrect(dp));
+            else if(st.size()>0&&st.top().first>=a[i])
+            {
+                while(st.size()>0&&st.top().first>=a[i])
+                {
+                    st.pop();
+                }
+                if(st.size()==0)
+                {
+                    v.push_back(-1);
+                }
+                else
+                {
+                    v.push_back(st.top().second);
+                }
+            }
+            st.push({a[i],i});
         }
-        return res;
+        return v;
+    }
+      vector<int>NSR(vector<int> a,int n)
+    {
+        vector<int>v;
         
+        stack<pair<int,int>>st;
+        for(int i=n-1;i>=0;i--)
+        {
+            if(st.size()==0)
+            v.push_back(n);
+            else if(st.size()>0&&st.top().first<a[i])
+            {
+                v.push_back(st.top().second);
+            }
+            else if(st.size()>0&&st.top().first>=a[i])
+            {
+                while(st.size()>0&&st.top().first>=a[i])
+                {
+                    st.pop();
+                }
+                if(st.size()==0)
+                {
+                    v.push_back(n);
+                }
+                else
+                {
+                    v.push_back(st.top().second);
+                }
+            }
+            st.push({a[i],i});
+        }
+        reverse(v.begin(),v.end());
+        return v;
+    }
+    int MAH(vector<int>& arr) {
+        int n=arr.size();
+        vector<int>a=NSL(arr,n);
+        vector<int>b=NSR(arr,n);
+        vector<int>width(n);
+        for(int i=0;i<n;i++)
+        {
+            width[i]=b[i]-a[i]-1;
+        }
+        int ans=0;
+        for(int i=0;i<n;i++)
+        {
+            ans=max(ans,width[i]*arr[i]);
+        }
+        return ans;
+    }
+    
+    int maximalRectangle(vector<vector<char>>& v) {
+        
+        vector<int> l(v[0].size(), 0);
+        int mx = 0;
+        for(int i=0;i<v.size();i++)
+        {
+            for(int j=0;j<v[0].size();j++)
+            {
+                if(v[i][j]=='0')
+                   l[j]=0;
+                else
+                {
+                    l[j]=l[j]+1;
+                }
+            }
+            mx=max(mx,MAH(l));
+        }
+         return mx;  
     }
 };
