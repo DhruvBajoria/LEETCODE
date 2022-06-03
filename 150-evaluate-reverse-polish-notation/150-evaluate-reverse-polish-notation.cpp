0454de92ea85bb3424952028372a1e53fedeca1d
Dhruv <71303408+DhruvBajoria@users.noreply.github.com>
2022-06-03 11:49:48 +0530

    Time: 11 ms (78.04%), Space: 11.7 MB (99.72%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.cpp
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.cpp
@@ -1,6 +1,5 @@
 class Solution {
 public:
-   
     int evalRPN(vector<string>& tokens) {
     stack<int>s;
         for(auto str:tokens){
@@ -8,8 +7,7 @@ public:
             if(str!="+" and str!="-" and str!="*" and str!="/"){
                 s.push(stoi(str));
             }
-			// if it is operator pop 2 elements and perform the required operation
-			// and push the result to the stack
+			
             else if(str=="+"){
                 int num1=s.top();s.pop();
                 int num2=s.top();s.pop();
