4b6209635a836432cc8cf93960d8faaf3c665f07
Dhruv <71303408+DhruvBajoria@users.noreply.github.com>
2022-06-17 12:59:47 +0530

    Time: 32 ms (92.50%), Space: 71.3 MB (90.52%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -10,41 +10,43 @@
  */
 class Solution {
 public:
-
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-      
-        ListNode* curr=NULL,*res=NULL,*temp=NULL;
-        int sum=0,c=0;
-        
-        
-        while(l1||l2)
+    ListNode* reverse(ListNode*head)
+    {
+        ListNode *prev=NULL,*next=NULL,*curr=head;
+        while(curr)
         {
-            sum=c+(l1?l1->val:0)+(l2?l2->val:0);
-            c=(sum>=10?1:0);
-            sum=sum%10;
-            temp=new ListNode(sum);
-            if(res==NULL)
-            {
-                res=temp;
-                curr=res;
-            }
-            else{
-                
-                curr->next=temp;
-                curr=curr->next;
-            }
-            if(l1)
-                l1=l1->next;
-            if(l2)
-                l2=l2->next;
+            next=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=next;
+            
         }
-       if(c>0)
-       {
-           temp=new ListNode(c);
-           curr->next=temp;
-           curr=temp;
-       }
-       
-        return res;
+        return prev;
+    }
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode *dummy=new ListNode(-1);
+        ListNode *temp=dummy;
+        int c=0,s=0;
+        while(l1||l2||c)
+        {
+            if(l1)
+            {
+                s+=l1->val;
+                l1=l1->next;
+            }
+               if(l2)
+            {
+                s+=l2->val;
+                l2=l2->next;
+            }
+            s+=c;
+            c=s/10;
+            s=s%10;
+            ListNode *help=new ListNode(s);
+            temp->next=help;
+            temp=temp->next;
+            s=0;
+        }
+        return (dummy->next);
     }
 };
