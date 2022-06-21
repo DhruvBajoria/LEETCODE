9f8fe6498c0981f1faca7548a1c4192571d590b2
Dhruv <71303408+DhruvBajoria@users.noreply.github.com>
2022-06-21 16:28:10 +0530

    Added solution - LeetHub

--- Given a linked list of 0s, 1s and 2s, sort it. - GFG/given-a-linked-list-of-0s-1s-and-2s-sort-it..cpp
+++ Given a linked list of 0s, 1s and 2s, sort it. - GFG/given-a-linked-list-of-0s-1s-and-2s-sort-it..cpp
@@ -35,7 +35,7 @@ class Solution
     //Function to sort a linked list of 0s, 1s and 2s.
     Node* segregate(Node *head) {
         int arr[3]={0};
-        Node *temp=head;
+        Node* temp=head;
         while(temp)
         {
             arr[temp->data]++;
@@ -44,13 +44,14 @@ class Solution
         temp=head;
         for(int i=0;i<3;i++)
         {
-            while(arr[i]--)
-            {
-                temp->data=i;
-                temp=temp->next;
-            }
+           while(arr[i]--)
+           {
+               temp->data=i;
+               temp=temp->next;
+           }
         }
-        return head;// Add code here
+        return head;
+        // Add code here
         
     }
 };
