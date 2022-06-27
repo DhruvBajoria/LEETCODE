d06cc718921e2fa83398fff86dff8c7276e6473f
Dhruv <71303408+DhruvBajoria@users.noreply.github.com>
2022-06-27 14:59:29 +0530

    Added solution - LeetHub

--- Minimum Swaps to Sort - GFG/minimum-swaps-to-sort.cpp
+++ Minimum Swaps to Sort - GFG/minimum-swaps-to-sort.cpp
@@ -11,29 +11,26 @@ class Solution
     //Function to find the minimum number of swaps required to sort the array. 
 	int minSwaps(vector<int>&nums)
 	{
-	    int n=nums.size();
+	    int swp=0;
 	    vector<pair<int,int>>v;
-	    for(int i=0;i<n;i++)
+	    for(int i=0;i<nums.size();i++)
 	    {
 	        v.push_back({nums[i],i});
-	        
 	    }
 	    sort(v.begin(),v.end());
-	    int cnt=0;
-	    for(int i=0;i<n;i++)
+	    for(int i=0;i<v.size();i++)
 	    {
-	        if(i==v[i].second)
+	        if(v[i].second==i)
 	        continue;
 	        else
 	        {
-	            cnt++;
+	            swp++;
 	            swap(v[i],v[v[i].second]);
 	            i--;
 	        }
 	    }
-	    return cnt;
-	  // Code here
-	
+	    return swp;
+	    // Code here
 	}
 };
 
