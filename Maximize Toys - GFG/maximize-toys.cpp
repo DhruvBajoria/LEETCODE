0eb0d88d58b9cebda116fc0cb57f46ac0460f254
Dhruv <71303408+DhruvBajoria@users.noreply.github.com>
2022-06-30 11:52:25 +0530

    Added solution - LeetHub

--- Maximize Toys - GFG/maximize-toys.cpp
+++ Maximize Toys - GFG/maximize-toys.cpp
@@ -12,17 +12,17 @@ class Solution{
 public:
     int toyCount(int N, int K, vector<int> arr)
     {
+        int cnt=0;
         sort(arr.begin(),arr.end());
         int sum=0;
-        
         for(int i=0;i<N;i++)
         {
             sum+=arr[i];
             if(sum>K)
             return i;
         }
-      
-      return N;
+        return N;
+        // code here
     }
 };
 
