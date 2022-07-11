1d9a41a2047f724898d7c41bc36edf6aa08a70da
Dhruv <71303408+DhruvBajoria@users.noreply.github.com>
2022-07-12 01:28:16 +0530

    Added solution - LeetHub

---
+++ Remove all occurences of duplicates in a linked list - GFG/remove-all-occurences-of-duplicates-in-a-linked-list.cpp
@@ -0,0 +1,118 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+/* Link list node */
+struct Node {
+	int data;
+	struct Node *next;
+	Node(int x) {
+		data = x;
+		next = NULL;
+	}
+};
+
+ // } Driver Code Ends
+//User function Template for C++
+
+/* Linked List node structure
+    
+struct Node {
+	int data;
+	struct Node *next;
+};
+
+*/
+
+class Solution
+{
+    public:
+    Node* removeAllDuplicates(struct Node* head)
+    {
+        unordered_map<int,int>mp;
+    Node* curr=head;
+    while(curr)
+    {
+        mp[curr->data]++;
+        curr=curr->next;
+    }
+    Node* hed=NULL,*temp=NULL;
+    curr=head;
+    while(curr)
+    {
+        if(mp[curr->data]==1)
+        {
+            if(hed==NULL)
+            {
+                hed=curr;
+                temp=hed;
+            }
+            else
+            {
+                temp->next=curr;
+                temp=temp->next;
+            }
+        }
+        curr=curr->next;
+    }
+    temp->next=NULL;
+    return hed;
+        //code here
+    }
+};
+
+// { Driver Code Starts.
+/* Function to print linked list */
+void printList(struct Node *head)
+{
+	struct Node *temp = head;
+	while (temp != NULL)
+	{
+		printf("%d ", temp->data);
+		temp = temp->next;
+	}
+}
+
+
+
+// Driver program to test above functions
+int main()
+{
+	int T;
+	cin >> T;
+
+
+	while (T--)
+	{
+		int N;
+
+		cin >> N ;
+
+		Node *head = NULL;
+		Node *temp = head;
+
+		for (int i = 0; i < N; i++) {
+			int data;
+			cin >> data;
+			if (head == NULL)
+				head = temp = new Node(data);
+			else
+			{
+				temp->next = new Node(data);
+				temp = temp->next;
+			}
+		}
+
+		Solution ob;
+		head = ob.removeAllDuplicates(head);
+		printList(head);
+
+		cout << "\n";
+
+
+	}
+	return 0;
+}
+  // } Driver Code Ends
