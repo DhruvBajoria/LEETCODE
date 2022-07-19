195f311f69a321a06b3e080a436eaf41a2ab6194
Dhruv <71303408+DhruvBajoria@users.noreply.github.com>
2022-07-19 17:08:30 +0530

    Time: 1076 ms (42.51%), Space: 163 MB (66.90%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,7 +1,25 @@
 class Solution {
 public:
     
-    
+    bool haspath(vector<vector<int>>&graph,int s,int d,vector<int>&vis)
+    {
+        if(s==d)
+            return true;
+        if(vis[s]==1)
+            return false;
+        vis[s]=1;
+        for(int i=0;i<graph[s].size();i++)
+        {
+            if(vis[graph[s][i]]==0)
+            {
+                bool nhaspath=haspath(graph,graph[s][i],d,vis);
+                if(nhaspath)
+                    return true;
+            }
+        
+        }
+        return false;
+    }
     
     
     bool validPath(int n, vector<vector<int>>& edges, int s, int d) {
@@ -13,25 +31,7 @@ public:
             graph[edges[i][1]].push_back(edges[i][0]);
         }
         vector<int>vis(n,0);
-        queue<int>q;
-        q.push(s);
-        vis[s]=1;
-        while(!q.empty())
-        {
-            int node =q.front();
-            q.pop();
-            if(node==d)
-                return true;
-            for(int i=0;i<graph[node].size();i++)
-            {
-                if(vis[graph[node][i]]==0)
-                {
-                    q.push(graph[node][i]);
-                    vis[graph[node][i]]=1;
-                }
-            }
-        }
-      
-        return false;
+        return haspath(graph,s,d,vis);
+    
     }
 };
