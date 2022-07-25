054d759101ec87880c503c0c38164323896d89e3
Dhruv <71303408+DhruvBajoria@users.noreply.github.com>
2022-07-25 19:58:03 +0530

    Time: 35 ms (95.26%), Space: 12.4 MB (56.56%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     void solve(int i,int j,int n,int m,vector<vector<char>>& grid)
     {
-        if(i<0||i>=n||j<0||j>=m||grid[i][j]!='1')
+        if(i<0||i>=n||j<0||j>=m||grid[i][j]=='0')
             return;
-        grid[i][j]='2';
+        grid[i][j]='0';
         
         solve(i+1,j,n,m,grid);
         solve(i-1,j,n,m,grid);
